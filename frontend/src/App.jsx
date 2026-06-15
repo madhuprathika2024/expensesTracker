38257d6f3cbc27748dc607679756725a67c3ee0a
Madhu <madhukarthick2024@gmail.com>
2026-06-15 14:27:35 +0530

    Fix Vercel build error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
@@ -12,8 +12,8 @@ function App() {
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          {/* <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" /> */}
         </div>
         <div>
           <h1>Get started</h1>
