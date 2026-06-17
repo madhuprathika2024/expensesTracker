fbdec01bfe39a46d6ebc8b12e20e229c52fa53bf
Madhu <madhukarthick2024@gmail.com>
2026-06-17 11:48:52 +0530

    message

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api/atm";
+const API = "https://expensestracker-1-10is.onrender.com/api/atm";
 
 export const signup = (data) => {
   return axios.post(`${API}/signup`, data);
